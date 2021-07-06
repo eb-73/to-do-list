a9097498484e32ba04777750aa33183886dcd266
eb-73 <ebrahim.baghbani73@gmail.com>
2021-07-07 00:00:53 +0430

    complete v-1

---
+++ home.js
@@ -0,0 +1,48 @@
+const div=document.querySelector("div.items");
+const nitem=document.querySelector("#item");
+const search=document.querySelector("#searchb");
+const items=[];
+let itemsearch="";
+nitem.addEventListener("keydown",function(e){
+    if(e.key==="Enter"){
+        add();
+    }
+    
+});
+
+div.addEventListener("click",function(e){
+    if(e.target.nodeName==="IMG"){
+        let delItem;
+        let index;
+        e.target.parentElement.remove();
+        delItem=e.target.parentElement.querySelector("h5").innerText;
+        index=items.indexOf(delItem);
+        items.splice(index,1);
+    }
+});
+
+
+search.addEventListener("input",function(e){
+   items.map(d => {div.querySelectorAll("div")[items.indexOf(d)].style.display="flex"})
+   itemsearch=search.value;
+   items.map(x => {
+       if(!x.includes(itemsearch)){
+        div.querySelectorAll("div")[items.indexOf(x)].style.display="none";
+       }
+    });
+
+});
+
+const add= ()=>{
+    const content=nitem.value;
+    items.push(content);
+    let innerDiv=document.createElement("div");
+    const h=document.createElement("h5");
+    const img=document.createElement("img");
+    h.innerText=content;
+    innerDiv.append(h);
+    img.src="trash.png";
+    innerDiv.append(img);
+    div.append(innerDiv);
+    nitem.value=" ";
+}
